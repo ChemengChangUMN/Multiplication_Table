5a242cb6743444eb46bed00c5495db0f92236d28
chemengchang <chan2166@umn.edu>
2025-09-29 19:26:34 -0500

    Added rows, cols, and generate button to create multipulcation table on browser using a mix of javascript, css, and html.

--- javascript.js
+++ javascript.js
@@ -49,3 +49,43 @@ let timesTable = (to) => {
 timesTable(4)
 timesTable(12)
 
+
+function getRowDesired(row) {
+    // console.log(Number(document.querySelector('.rows input').value))
+    //console.log(Number(document.querySelectorAll('.rows input')[0].value))
+    //console.log(Number(document.getElementById('rows').value))
+    return Number(document.getElementById("rows").value)
+}
+
+function getcolummnsdesired() {
+    return Number(document.getElementById("cols").value)
+}
+
+
+function createWithHtmlTable(rows, cols) {
+    let string // define a variable called string
+    if (rows && cols) { // if rows and columns were provided
+        string = "<table>\n"
+
+        for (let row = 1; row <= rows; row++) {
+            string += "<tr>\n"
+
+            for (let col = 1; col <= cols; col++) {
+                string += "<td>"
+                string += row * col
+                string += "</td>\n"
+            }
+
+            string += "</tr>\n"
+        }
+
+        string += "</table>\n"
+    } else {
+        string = 'Provide some inputs'
+    }
+
+    document.getElementById("output").innerHTML = string;
+
+}
+
+// createWithHtmlTable(10, 40)
