79142fd1ad5ecb21e9034c4c93da3adc6284969d
chemengchang <chan2166@umn.edu>
2025-10-15 16:34:37 -0500

    Copying provided code from REPO and creating a new READ ME

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# Profile
-### Hello!
+# Rendered-In-Flexbox
+### What I learned and struggled with
 
-My name is Chemeng Chang. I am a senior in Graphic Design at the University of Minnesota Twin Cities.
-I am interested in Games, Anime, Drawing, and Manga/Manhwa.
+Honestly, I struggled with this entire project. I was clueless where to start and trying to get a foothold in this project was hard. At some point I did decide to just create the table. But it  was not what we were trying to achieve. So in the end, I just ended up using the code repo that we were provided. 
+
+What I learned from this project was how to use flexbox to flex the classes and objects to how I wanted them to look.
 
--- javascript.js
+++ javascript.js
@@ -120,7 +120,30 @@ function getRowDesired() {
     return parseInt(document.getElementById('rows').value);
 }
 
-function createWithFlexbox(rows, cols) {
-    let string: =''
-
+function getColumnDesired() {
+    return parseInt(document.getElementById('cols').value);
 }
+
+function createWithFlexbox(rows, cols) {
+    let string=''
+
+    let row = 1
+    do {
+        string += "<div class='row'>";
+        string += row
+
+        let col = 1
+        do {
+            string += "<div class='col'>";
+            string += row * col
+            string += "</div>"
+
+            col = col + 1
+        } while (col <= cols)
+        string += "</div>\n"
+
+        row = row + 1;
+    }while(row <= rows)
+
+    document.getElementById('output').innerHTML = string
+        }
